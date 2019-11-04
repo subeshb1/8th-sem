9cea75f9f1e1d52fee8849d7621e8888d04f0ae9
subeshb1 <subeshb1@gmail.com>
2019-11-04 07:53:00 +0545

    Add lab assignment

---
+++ DataMinning/lab/lab1.py
@@ -0,0 +1,23 @@
+# Sum elements in an array
+def _sum(arr):
+  sum = 0
+  for item in arr:
+    sum += item
+  return sum
+
+int_array = [1,2,3,4,5]
+
+print(sum(int_array))
+
+# Find common in array
+
+def common(arr1, arr2):
+  arr3 = []
+  for item in arr1:
+    if item in arr2:
+      arr3.append(item)
+  return arr3
+
+set1 = ['a','b','c', 'd', 'e']
+set2 = ['a','x','b', 'y']
+print(common(set1, set2))
