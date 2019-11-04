4b59c38d14e5c83759beb1bc3c80fd33a64c0884
subeshb1 <subeshb1@gmail.com>
2019-11-04 08:00:44 +0545

    Add lab assignment

--- DataMinning/lab/lab1.py
+++ DataMinning/lab/lab1.py
@@ -1,4 +1,4 @@
-# Sum elements in an array
+# 1 Sum elements in an array
 def _sum(arr):
   sum = 0
   for item in arr:
@@ -9,7 +9,7 @@ int_array = [1,2,3,4,5]
 
 print(_sum(int_array))
 
-# Find common in array
+# 2 Find common in array
 
 def common(arr1, arr2):
   arr3 = []
@@ -21,3 +21,15 @@ def common(arr1, arr2):
 set1 = ['a','b','c', 'd', 'e']
 set2 = ['a','x','b', 'y']
 print(common(set1, set2))
+
+# 3 Reverse
+def reverse(arr1):
+  length = len(arr1)
+  for i in range(0,length/2):
+    temp = arr1[i]
+    arr1[i] = arr1[length-1-i]
+    arr1[length-1-i] = temp
+  return arr1
+
+print(reverse(set1))
+    
