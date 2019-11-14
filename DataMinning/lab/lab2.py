8b4a40709994aa9a841ee4f58516d2162de23b06
subeshb1 <subeshb1@gmail.com>
2019-11-14 08:03:10 +0545

    Add lab

---
+++ DataMinning/lab/lab2.py
@@ -0,0 +1,28 @@
+transactions = [
+  ['A', 'B', 'C', 'E'],
+  ['E', 'F', 'A'],
+  ['B', 'E'],
+  ['B', 'C', 'F'],
+]
+
+def merge_array_of_arrays(arrs):
+  new_arr = []
+  for arr in arrs:
+    new_arr = new_arr + arr
+  return new_arr
+
+
+def support_count(items, transaction):
+  support_count = {}
+  for item in items:
+    if item not in support_count:
+      support_count[item] = 0
+    for transaction in transactions:
+      if item in transaction:
+        support_count[item] += 1
+  return support_count
+
+
+item_set = set(merge_array_of_arrays(transactions))
+
+print(support_count(item_set, transactions))
